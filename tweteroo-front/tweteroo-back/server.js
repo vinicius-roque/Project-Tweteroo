a5b0f368cdff5f6279e243ae27ade4d07d5edd82
Vinícius Roque <viniciusfr6@gmail.com>
2022-08-25 22:11:59 -0300

    docs and feat(tweteroo-back/server.js): added server and its route

---
+++ tweteroo-front/tweteroo-back/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from "cors";
+
+const server = express(); 
+server.use(express.json());
+server.use(cors());
+
+
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
+server.listen(5000);
