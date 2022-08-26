d1e3bb8230bc5f59812ca2278d09b646bdb17db8
Vinícius Roque <viniciusfr6@gmail.com>
2022-08-26 17:01:17 -0300

    feat(server.js): including first post requisit

--- tweteroo-back/server.js
+++ tweteroo-back/server.js
@@ -5,6 +5,16 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const user = [];
+const tweet = [];
+
+server.post('/sign-up', (req, res) => {
+  const signUp = req.body;
+  
+  user.push(signUp);
+  
+  res.send("OK");
+});
 
 server.get('/', (req, res) => {
   res.send('Hello World');
